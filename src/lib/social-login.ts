0a60a071043454eb9d00afd5a4439a3a3ce401c3
自由的世界人 <3196812536@qq.com>
2025-09-08 17:44:53 +0800

    fix: social oauth error

--- src/lib/social-login.ts
+++ src/lib/social-login.ts
@@ -3,11 +3,13 @@ import {authClient} from "./auth-client";
 export const signinGithub = async () => {
     return await authClient.signIn.social({
         provider: "github",
+        callbackURL: "/chat",
     });
 };
 
 export const signinGoogle = async () => {
     return await authClient.signIn.social({
         provider: "google",
+        callbackURL: "/chat",
     });
 };
